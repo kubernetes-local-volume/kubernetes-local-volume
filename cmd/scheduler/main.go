0701e2c1151e6e81670aa72fd782188b32168c58
xuxingwen <xuxingwen@jd.com>
2020-03-09 19:44:42 +0800

    first commit

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("scheduler")
+	fmt.Println("vim-go")
 }
--- pkg/lvm/utils.go
+++ pkg/lvm/utils.go
@@ -27,7 +27,7 @@ import (
 	"strings"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
-	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
+	"github.com/kubernetes-local-volume/kubernetes-local-volume/pkg/utils"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
